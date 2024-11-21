a744a367f4f3dcee0bdae349d4b25dcb1648edfc
Arthur Munhoz <arthur@peek.us>
2024-11-21 18:28:12 -0500

    Adding final information on lib

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # axios-abort-controller-hook
+
+This is a lib that provides a custom React hook that simplifies the usage of Axios with the AbortController API.
+It allows you to easily cancel ongoing HTTP GET requests, and helping to prevent memory leaks and improve the performance of your application. The hook also cancels any ongong requests if your component unmounts.
+
+## Installation
+
+```bash
+npm install axios-abort-controller-hook
+```
+
+## Usage
+
+Here's a simple example of how to use the `axios-abort-controller-hook` in your React component:
+
+```jsx
+import React, { useEffect } from 'react';
+import useAxiosAbortController from 'axios-abort-controller-hook';
+
+const ItemsList = () => {
+  const [page, setPage] = useState(0);
+
+  const { api, data, isLoading, error, abort } = useGetWithAbort({
+    "https://example-url.com/items",
+    {
+      "params": {
+        "page": 1,
+        "amount": 20,
+      }
+    }
+  });
+
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <ul>
+          {data.items.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+      <button onClick={() => setPage(prevPage => prevPage + 1)}>Next Page</button>
+      <button onClick={abort}>Cancel Request</button>
+    </div>);
+  )
+}
---
+++ src/hooks/useGetWithAbort.tsx
@@ -0,0 +1,68 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+/**
+ * This hook is used to fetch data from the given URL with the given config.
+ * It also provides a function to abort the fetch request if it is still pending.
+ * 
+ * @param url string to fetch data from
+ * @param config object to pass to the fetch request containing headers, body, params, etc.
+ * @returns the api function, the data, the loading state, the error, and the abort function
+ */
+export const useGetWithAbort = (url: string, config: AxiosRequestConfig) => {
+
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  // This ref is used to store the AbortController instance
+  const controller = useRef<AbortController | null>(null);
+
+  // This function is used to fetch the data from the given URL.
+  const api = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      if (!controller.current) {
+        controller.current = new AbortController();
+      }
+
+      const response = await axios.get(url, {
+        signal: controller.current.signal,
+        ...config
+      });
+
+      setData(response.data);
+      setIsLoading(false);
+    }
+    catch (error: any) {
+      if (error === 'AbortError') {
+        console.log('Fetch aborted');
+      } else {
+        setError(error);
+      }
+
+      setIsLoading(false);
+    }
+  }, [url, config]);
+
+  // This function is used to abort the fetch request if it is still pending
+  const abort = () => {
+    if (controller.current) {
+      controller.current.abort();
+      setIsLoading(false);
+    }
+
+    controller.current = new AbortController();
+  };
+
+  // This effect is used to abort the fetch request if the component is unmounted
+  useEffect(() => {
+    return () => {
+      if (controller.current)
+        controller.current.abort();
+    };
+  }, []);
+
+  return { api, data, isLoading, error, abort };
+};
